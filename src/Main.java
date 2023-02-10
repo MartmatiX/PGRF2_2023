b169c2a3adccaaff35f61398e39c7572dbdcb267
MartmatiX <martin.malir@gmail.com>
2023-02-10 20:20:06 +0100

    Finished main method from cv2

--- src/Main.java
+++ src/Main.java
@@ -13,15 +13,19 @@ public class Main {
         colRaster.setPixel(3, 2, new Col(0, 0, 255));
         colRaster.getPixel(3, 2).ifPresent(System.out::println);
         colRaster.setPixel(3, 2, new Col(0, 255, 0));
-        colRaster.getPixel(3, 2).ifPresent(System.out::println);
+        colRaster.getPixel(3, 2).ifPresent(System.out::println); // (0.0, 1.0, 0.0, 1.0)
         colRaster.clear();
         System.out.println();
 
         final ZBuffer zBuffer = new ZBuffer(colRaster);
         colRaster.getPixel(3, 2).ifPresent(System.out::println);
-        zBuffer.setPixel(3, 2, 0.5, new Col(0, 0, 255));
-        colRaster.getPixel(3, 2).ifPresent(System.out::println);
         zBuffer.setPixel(3, 2, 0.5, new Col(255, 0, 0));
+        colRaster.getPixel(3, 2).ifPresent(System.out::println);
+        zBuffer.setPixel(3, 2, 0.2, new Col(0, 0, 255));
+        colRaster.getPixel(3, 2).ifPresent(System.out::println);
+        zBuffer.setPixel(3, 2, 0.7, new Col(0, 255, 0));
+        colRaster.getPixel(3, 2).ifPresent(System.out::println); // should be (0.0, 0.0, 1.0, 1.0)
+
     }
 
 }
