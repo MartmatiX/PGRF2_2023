9b5be6b9541fb5dc0a8eb5235bc13f606c9cf1bb
MartmatiX <martin.malir@gmail.com>
2023-02-10 21:27:11 +0100

    Implementation of setPixel and clear methods

--- src/raster_data/ZBuffer.java
+++ src/raster_data/ZBuffer.java
@@ -13,11 +13,23 @@ public class ZBuffer {
         this.depthRaster = new DepthRaster(10, 10);
     }
 
-    public void setPixel(int x, int y, double z, Col pixe) {
-        // TODO: finish
+    public void setPixel(int x, int y, double z, Col pixel) {
+        if (colRaster.isValidAddress(x, y) && depthRaster.isValidAddress(x, y)) {
+            depthRaster.setPixel(x, y, z);
+            colRaster.setPixel(x, y, pixel);
+        }
     }
 
+    /**
+     * Method cycles through entire panel and for each pixel sets its value in depthRaster to 1.0
+     * and for every pixel in colRaster to default color
+     */
     public void clear() {
-        // TODO: finish
+        for (int i = 0; i < depthRaster.getWidth(); i++) {
+            for (int j = 0; j < depthRaster.getHeight(); j++) {
+                depthRaster.setPixel(i, j, 1.0);
+                colRaster.setPixel(i, j, new Col(0, 0, 0));
+            }
+        }
     }
 }
