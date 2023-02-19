b95573f9d88673bc335de43d0eec235bcdf59815
MartmatiX <martin.malir@gmail.com>
2023-02-19 17:32:17 +0100

    drawSecondHalf finished, drawTriangle function implementation

--- src/rasterops/Triangler.java
+++ src/rasterops/Triangler.java
@@ -5,7 +5,6 @@ import object_data.Vertex;
 import raster_data.Raster;
 import transforms.Col;
 
-import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -39,13 +38,10 @@ public class Triangler {
     }
 
     private List<Vertex> sortedAlt(Vertex v1, Vertex v2, Vertex v3) {
-        return Stream.of(v1, v2, v3).sorted(new Comparator<Vertex>() {
-            @Override
-            public int compare(Vertex o1, Vertex o2) {
-                if (o1.getPosition().getY() < o2.getPosition().getY()) return -1;
-                else if (o1.getPosition().getY() > o2.getPosition().getY()) return 1;
-                return 0;
-            }
+        return Stream.of(v1, v2, v3).sorted((o1, o2) -> {
+            if (o1.getPosition().getY() < o2.getPosition().getY()) return -1;
+            else if (o1.getPosition().getY() > o2.getPosition().getY()) return 1;
+            return 0;
         }).toList();
     }
 
@@ -70,14 +66,80 @@ public class Triangler {
     }
 
     private void drawSecondHalf(Vertex a, Vertex b, Vertex c) {
-        // TODO: finish this
-        // TODO: pokusit se sjednotit do jedne tridy s drawFirstHalf
+        final int yMin = (int) b.getPosition().getY();
+        final double yMax = c.getPosition().getY();
+        for (int y = yMin; y < yMax; y++) {
+            final double t1 = (y - a.getPosition().getY()) / (c.getPosition().getY() - a.getPosition().getY());
+            final double t2 = (y - b.getPosition().getY()) / (c.getPosition().getY() - b.getPosition().getY());
+            final Vertex v1 = lerp.compute(a, c, t1);
+            final Vertex v2 = lerp.compute(b, c, t2);
+            final Vertex vMin = (v1.getPosition().getX() < v2.getPosition().getX()) ? v1 : v2;
+            final Vertex vMax = (vMin == v1) ? v2 : v1;
+            final int xMin = (int) vMin.getPosition().getX();
+            final double xMax = vMax.getPosition().getX();
+            for (int x = xMin; x < xMax; x++) {
+                final double t = (x - xMin) / (xMax - xMin);
+                final Vertex v = lerp.compute(vMin, vMax, t);
+                image.setPixel(x, y, v.getColor());
+            }
+        }
+    }
+
+    private void drawTriangle(Vertex a, Vertex b, Vertex c) {
+        double yA = a.getPosition().getY();
+        double yB = b.getPosition().getY();
+        double yC = c.getPosition().getY();
+
+        final double distanceYCA = yC - yA;
+        final double distanceYCB = yC - yB;
+        final double distanceYBA = yB - yA;
+
+        for (int y = (int) yA; y < yB; y++) {
+            final double t1 = (y - yA) / distanceYCA;
+            final double t2 = (y - yA) / distanceYBA;
+
+            final Vertex v1 = lerp.compute(a, c, t1);
+            final Vertex v2 = lerp.compute(a, b, t2);
+
+            final Vertex vMin = (v1.getPosition().getX() < v2.getPosition().getX()) ? v1 : v2;
+            final Vertex vMax = (v1.getPosition().getX() < v2.getPosition().getX()) ? v2 : v1;
+
+            int xMin = (int) vMin.getPosition().getX();
+            double xMax = vMax.getPosition().getX();
+
+            xMax = Math.min(xMax, image.getWidth());
+
+            for (int x = xMin; x < xMax; x++) {
+                final double t = (x - xMin) / (xMax - xMin);
+                final Vertex v = lerp.compute(vMin, vMax, t);
+                image.setPixel(x, y, v.getColor());
+            }
+        }
+
+        for (int y = (int) yB; y < yC; y++) {
+            final double t1 = (y - yA) / distanceYCA;
+            final double t2 = (y - yB) / distanceYCB;
+
+            final Vertex v1 = lerp.compute(a, c, t1);
+            final Vertex v2 = lerp.compute(b, c, t2);
+
+            Vertex vMin = (v1.getPosition().getX() < v2.getPosition().getX()) ? v1 : v2;
+            final Vertex vMax = (v1.getPosition().getX() < v2.getPosition().getX()) ? v2 : v1;
+
+            final int xMin = (int) vMin.getPosition().getX();
+            double xMax = vMax.getPosition().getX();
+
+            for (int x = xMin; x < xMax; x++) {
+                final double t = (x - xMin) / (xMax - xMin);
+                final Vertex v = lerp.compute(vMin, vMax, t);
+                image.setPixel(x, y, v.getColor());
+            }
+        }
     }
 
     public void draw(Vertex v1, Vertex v2, Vertex v3) {
         final List<Vertex> ordered = sorted(v1, v2, v3);
-        drawFirstHalf(ordered.get(0), ordered.get(1), ordered.get(2));
-        drawSecondHalf(ordered.get(0), ordered.get(1), ordered.get(2));
+        drawTriangle(ordered.get(0), ordered.get(1), ordered.get(2));
     }
 
 }
